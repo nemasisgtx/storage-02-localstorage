aa04f824f74edbb8fe9ae85281628c37ca9b34bc
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-18 14:46:27 +0530

    Qol v1.1.2

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -19,7 +19,7 @@ const ExpenseItem = (props) => {
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button value = {clickHandler}>EXP</button>
+        <button onClick={clickHandler}>EXP</button>
       </div>
     </Card>
   );
