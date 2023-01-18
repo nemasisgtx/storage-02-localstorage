0eae4477b8e44864f27d457531e6f95102e93788
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-19 00:36:27 +0530

    Implemented 2-way binding with Filter function

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
   const addExpenseHandler = (expense) => {
     console.log('In App.js');
-    console.log(expenses);
+    console.log(expense);
   };
 
   return (
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import Card from "../UI/Card";
-import "./ExpenseForm.css";
+import { useState } from 'react';
+import Card from '../UI/Card';
+import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
-  const [enteredTitle, SetEnteredTitle] = useState("");
-  const [enteredDate, SetEnteredDate] = useState("");
-  const [enteredAmount, SetEnteredAmount] = useState("");
+  const [enteredTitle, SetEnteredTitle] = useState('');
+  const [enteredDate, SetEnteredDate] = useState('');
+  const [enteredAmount, SetEnteredAmount] = useState('');
 
   // const [userInput, setUserInput] = useState({
   //   enteredTitle: '',
@@ -37,50 +37,50 @@ const ExpenseForm = (props) => {
       date: new Date(enteredDate),
     };
     props.onSaveExpenseData(expenseData);
-    SetEnteredAmount("");
-    SetEnteredDate("");
-    SetEnteredTitle("");
+    SetEnteredAmount('');
+    SetEnteredDate('');
+    SetEnteredTitle('');
   };
 
   return (
-    <Card>
-      <div className="expense-form_outer">
-        <form onSubmit={submitHandler}>
-          <div className="new-expense__controls">
-            <div className="new-expense__control form-title">
-              <label className="form-label-title">Title: </label>
-              <input
-                type="text"
-                value={enteredTitle}
-                onChange={titleChangeHandler}
-              />
+      <Card>
+        <div className="expense-form_outer">
+          <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+              <div className="new-expense__control form-title">
+                <label className="form-label-title">Title: </label>
+                <input
+                  type="text"
+                  value={enteredTitle}
+                  onChange={titleChangeHandler}
+                />
+              </div>
+              <div className="new-expense__control form-amount">
+                <label className="form-label-amount">Amount: </label>
+                <input
+                  type="number"
+                  min="0.01"
+                  step="0.01"
+                  value={enteredAmount}
+                  onChange={amountChangeHandler}
+                />
+              </div>
+              <div className="new-expense__control form-date">
+                <label className="form-label-date">Date: </label>
+                <input
+                  className="form-inner-date-module"
+                  type="date"
+                  min="2019-01-01"
+                  max="2023-12-31"
+                  value={enteredDate}
+                  onChange={dateChangeHandler}
+                />
+              </div>
             </div>
-            <div className="new-expense__control form-amount">
-              <label className="form-label-amount">Amount: </label>
-              <input
-                type="number"
-                min="0.01"
-                step="0.01"
-                value={enteredAmount}
-                onChange={amountChangeHandler}
-              />
-            </div>
-            <div className="new-expense__control form-date">
-              <label className="form-label-date">Date: </label>
-              <input
-                className="form-inner-date-module"
-                type="date"
-                min="2019-01-01"
-                max="2023-12-31"
-                value={enteredDate}
-                onChange={dateChangeHandler}
-              />
-            </div>
-          </div>
-          <button className="form-btn">Add Expense</button>
-        </form>
-      </div>
-    </Card>
+            <button className="form-btn">Add Expense</button>
+          </form>
+        </div>
+      </Card>
   );
 };
 
