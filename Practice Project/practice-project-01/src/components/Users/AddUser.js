325c2e3f522329100cd7760a5270705664041215
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-21 14:39:29 +0530

    Added Clean User Input functionality

--- Practice Project/practice-project-01/src/components/Users/AddUser.js
+++ Practice Project/practice-project-01/src/components/Users/AddUser.js
@@ -1,24 +1,51 @@
-import React from "react";
-import Buttons from "../UI/Button";
+import React from 'react';
+import { useState } from 'react';
+import Buttons from '../UI/Button';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 
-const AddUser = props => {
-    const addUserHandler = (event) =>{
-        event.preventDefault();
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const addUsernameHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const addAgeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
     }
-    // const username = props.event.target.value;
-    // const age = props.event.target.value;
-    return (
-        <Card className={classes.input}>
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input  id="username" type='text'></input>
-            <label htmlFor="age">Age</label>
-            <input  id="age" type='number'></input>
-            <Buttons type="submit">Add User</Buttons>
-        </form>
-        </Card>
-    );
-}
+    if (+enteredAge < 1) {
+      return;
+    }
+  };
+  console.log(enteredUsername, enteredAge);
+  setEnteredUsername('');
+  setEnteredAge('');
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          onChange={addUsernameHandler}
+          value={enteredUsername}
+          id="username"
+          type="text"
+        ></input>
+        <label htmlFor="age">Age</label>
+        <input
+          onChange={addAgeHandler}
+          value={enteredAge}
+          id="age"
+          type="number"
+        ></input>
+        <Buttons type="submit">Add User</Buttons>
+      </form>
+    </Card>
+  );
+};
 export default AddUser;
