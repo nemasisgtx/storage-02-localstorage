becc84d3e484b58b746e0e8b022c03490e6061bd
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-25 01:32:04 +0530

    Added Cart and added for closing backdrop/ modal

--- React Restaurant App/src/components/Layout/Header.js
+++ React Restaurant App/src/components/Layout/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => {
                 <h1>
                    Super Meals
                 </h1>
-                <HeaderCartButton/>
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
             <img src={mealsImage} alt="meals" />
