59304538e702959e87f163695119caec48cab150
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-25 18:04:10 +0530

    Fixed Bug

--- React Restaurant App/src/components/Meals/MealItem/MealItem.js
+++ React Restaurant App/src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,9 @@
-import classes from "./MealItem.module.css";
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
-
-  const price = `$${props.price.toFixed(2)}`
- 
+  const price = `$${props.price.toFixed(2)}`;
+  console.log(props);
 
   return (
     <li className={classes.meal}>
@@ -12,8 +12,11 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-     
+      <div>
+        <MealItemForm id={props.id} />
+      </div>
     </li>
   );
 };
+
 export default MealItem;
