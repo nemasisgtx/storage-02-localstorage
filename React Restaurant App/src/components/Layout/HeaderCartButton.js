6ace9df842904fd6a77d41c9ac713f22f533d1e2
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-27 00:32:23 +0530

    added reducer hook on CartProvider component

--- React Restaurant App/src/components/Layout/HeaderCartButton.js
+++ React Restaurant App/src/components/Layout/HeaderCartButton.js
@@ -2,6 +2,7 @@ import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
