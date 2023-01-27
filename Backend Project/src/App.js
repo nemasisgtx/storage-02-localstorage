27866aba94cb0271e080d30382cd90b2bbedd0d0
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-27 14:40:56 +0530

    implementing loading state while fetching API

--- Backend Project/src/App.js
+++ Backend Project/src/App.js
@@ -21,7 +21,10 @@ function App() {
   // ];
 
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json();
 
@@ -33,6 +36,8 @@ function App() {
         releaseDate: movies.release_date,
       };
     });
+    setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   // const fetchMoviesHandler = () => {
@@ -59,7 +64,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
