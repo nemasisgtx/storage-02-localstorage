00fa8f3907abcb229149636793eddc7e2d03a339
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-28 17:43:53 +0530

    Updated Modal

--- React Restaurant App/src/components/Cart/Cart.js
+++ React Restaurant App/src/components/Cart/Cart.js
@@ -4,7 +4,6 @@ import { useContext } from 'react';
 import cartContext from '../../Store/cart-context';
 
 const Cart = (props) => {
-  const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
   const cartCtx = useContext(cartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItem = cartCtx.items.length > 0;
@@ -24,14 +23,14 @@ const Cart = (props) => {
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
+        <span>{hasItem}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
           Checkout
         </button>
-        <button className={classes["button--alt"]} onClick={props.onClose}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
       </div>
