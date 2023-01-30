6171eb78d054034378dc24f4e9396cc7808d0b53
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-31 01:12:42 +0530

    trying different input field logic with redux

--- Redux Exp/src/components/Counter.js
+++ Redux Exp/src/components/Counter.js
@@ -1,9 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+  const [inputAmount, setInputAmount] = useState(0);
 
   const incrementCounterHandler = () => {
     dispatch({ type: "INCREMENT" });
@@ -12,16 +15,28 @@ const Counter = () => {
   const decrementCounterHandler = () => {
     dispatch({ type: "DECREMENT" });
   };
+  const increaseCounterHandler = () => {
+    dispatch({ type: "INCREASE", amount: inputAmount });
+  };
 
-  const toggleCounterHandler = () => {};
+  const increaseInputHandler = (event) => {
+    setInputAmount(event.target.value);
+  };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "TOGGLE" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementCounterHandler}>Increment Counter</button>
         <button onClick={decrementCounterHandler}>Decrement Counter</button>
+        <div>
+        <input onChange={increaseInputHandler} placeholder='Enter a Number'></input>
+        <button onClick={increaseCounterHandler}>Increase Counter by Input </button>
+        </div>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- Redux Exp/src/store/shop.js
+++ Redux Exp/src/store/shop.js
@@ -1,11 +1,20 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+const counterReducer = (state = initialState, action) => {
+  
   switch (action.type) {
-    case "INCREMENT":
-      return { counter: state.counter + 1 };
-    case "DECREMENT":
-      return { counter: state.counter - 1 };
+    case 'INCREMENT':
+      return { counter: state.counter + 1, showCounter: state.showCounter };
+    case 'DECREMENT':
+      return { counter: state.counter - 1, showCounter: state.showCounter };
+    case 'TOGGLE':
+      return { counter: state.counter, showCounter: !state.showCounter };
+    case 'INCREASE':
+      return {
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter,
+      };
     default:
       return state;
   }
