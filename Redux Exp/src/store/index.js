c115e0bdbae298fbd99d1c88215f1001adde7207
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-30 18:07:35 +0530

    Created a store and used it in parent

---
+++ Redux Exp/src/store/index.js
@@ -0,0 +1,14 @@
+import {createStore} from 'react';  
+
+const counterReducer = (state = {counter: 0}, action)   => {
+    switch (action.type) {  
+        case 'INCREMENT':  
+            return {counter: state.counter + 1};  
+        case 'DECREMENT':  
+            return {counter: state.counter - 1};  
+        default:  
+            return state;  
+    }
+}
+    const store = createStore(counterReducer);
+    export default store;
