443ffce34d2b5b8eb097e6bd8293084c687136df
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-31 16:01:03 +0530

    Added input storage using useState and reset form

--- Redux Exp/src/components/Auth.js
+++ Redux Exp/src/components/Auth.js
@@ -1,9 +1,22 @@
 import classes from './Auth.module.css';
+import { useState } from 'react';
 
 const Auth = () => {
+const [email , setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
   const formSubmitHandler = (e) => {
     e.preventDefault();
     console.log('form submitted');
+    setEmail('');
+    setPassword('');
   };
   return (
     <main className={classes.auth}>
@@ -11,13 +24,13 @@ const Auth = () => {
         <form onSubmit={formSubmitHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <input value={email} onChange={emailHandler} type='email' id='email' />
           </div>
           <div className={classes.control}>
             <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <input value={password} type='password' id='password' onChange={passwordHandler} />
           </div>
-          <button>Login</button>
+          <button >Login</button>
         </form>
       </section>
     </main>
