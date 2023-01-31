15ee8b0c619bbc135202b2814ff2adcc8f81ea01
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-01-31 18:40:10 +0530

    Added New Module

--- Redux Exp/src/components/UserProfile.js
+++ Redux Exp/src/components/UserProfile.js
@@ -9,3 +9,15 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
+
+// import classes from './UserProfile.module.css';
+
+// const UserProfile = () => {
+//   return (
+//     <main className={classes.profile}>
+//       <h2>My User Profile</h2>
+//     </main>
+//   );
+// };
+
+// export default UserProfile;
