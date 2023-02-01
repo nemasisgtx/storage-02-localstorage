e15eb06c8dfdbb792406db3c670d783bc6c9ebf7
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-02-02 02:00:43 +0530

    finding the bug

--- ReduxCart/Redux Shopping Cart/src/components/store/cart-slice.js
+++ ReduxCart/Redux Shopping Cart/src/components/store/cart-slice.js
@@ -28,10 +28,10 @@ const cartSlice = createSlice({
       },
     removeItemFromCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.find((item) => item.id === id);
+      const existingItem = state.items.find(item => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.items = state.items.filter((item) => item.id !== id);
+        state.items = state.items.filter(item => item.id !== id);
       } else {
         existingItem.quantity--;
         existingItem.totalPrice -= existingItem.price;
