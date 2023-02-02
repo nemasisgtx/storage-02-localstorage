a9a67385952299251a50849950585b85ac6ac2b9
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-02-02 12:57:12 +0530

    used Fetch API => FirebaseDB with useEffect

--- ReduxCart/Redux Shopping Cart/src/App.js
+++ ReduxCart/Redux Shopping Cart/src/App.js
@@ -2,9 +2,19 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://react-project-http-352bf-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart), 
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
--- ReduxCart/Redux Shopping Cart/src/components/Cart/CartItem.js
+++ ReduxCart/Redux Shopping Cart/src/components/Cart/CartItem.js
@@ -8,7 +8,7 @@ const CartItem = (props) => {
   const { title, quantity, total, price, id } = props.item;
 
   const removeItemHandler = () => {
-    dispatch(cartActions.removeItemFromCart( id ));
+    dispatch(cartActions.removeItemFromCart(id));
   };
   const addItemHandler = () => {
     dispatch(cartActions.addItemToCart({ id, title, price }));
