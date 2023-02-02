c8aff3dab9ad02d0e00988ed4e715f952a364571
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-02-02 18:33:31 +0530

    QOL Changes

--- ReduxCart/Redux Shopping Cart/src/components/store/cart-api-thunk.js
+++ ReduxCart/Redux Shopping Cart/src/components/store/cart-api-thunk.js
@@ -13,7 +13,7 @@ export const fetchCartData = () => {
       }
 
       const data = await response.json();
-
+      
       return data;
     };
 
--- ReduxCart/Redux Shopping Cart/src/components/store/ui-slice.js
+++ ReduxCart/Redux Shopping Cart/src/components/store/ui-slice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartIsVisible: false, notification: null },
+  initialState: { cartIsVisible: Boolean, notification: [] },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
