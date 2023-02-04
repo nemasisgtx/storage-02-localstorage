227a952d04acbf20699786cc0f665ba131f0bafe
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-02-04 17:16:18 +0530

    Added a show detail page and added module styling

---
+++ Bigger NextJs Project/pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+const MeetupDetails = (props) => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/f/f8/Roman_Baths%2C_Bath%2C_2017.jpg"
+      title="First Meetup"
+      address="Somewhere in Rome"
+      description="This is my first meetup."
+    />
+  );
+};
+export default MeetupDetails;
