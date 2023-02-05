5654fdb3d576be3b7c02fa8d23a239835e6acbc7
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-02-06 00:04:05 +0530

    finished api route with mongoDb

--- Bigger NextJs Project/pages/api/new-meetup.js
+++ Bigger NextJs Project/pages/api/new-meetup.js
@@ -1,10 +1,25 @@
 // /api/new-meetup
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { image, title, address, description } = data;
+    // const { image, title, address, description } = data;
+
+    MongoClient.connect(
+      'mongodb+srv://techadmin:retroTechdev@97@cluster0.rnedgjz.mongodb.net/test'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({message: 'Meetup Inserted!'});
   }
 };
 
