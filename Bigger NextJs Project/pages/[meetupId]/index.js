4185215b1f781fe0232eee97e6807b034b435e93
StrikerTech <52026453+nemasisgtx@users.noreply.github.com>
2023-02-06 15:10:56 +0530

    import fix

--- Bigger NextJs Project/pages/[meetupId]/index.js
+++ Bigger NextJs Project/pages/[meetupId]/index.js
@@ -1,5 +1,5 @@
 import { MongoClient, ObjectId } from "mongodb";
-
+import Head from "next/head";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails(props) {
